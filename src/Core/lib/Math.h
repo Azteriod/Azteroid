0d3aec6a3372dd655e955597f8cb3065b208cf66
Giovanny Andres Gongora Granada <gioyik@gmail.com>
2015-05-02 18:28:25 -0500

    Math header file

---
+++ src/Core/lib/Math.h
@@ -0,0 +1,31 @@
+/*
+ * Math.h
+ */
+
+#ifndef AZT_MATH_H
+#define AZT_MATH_H
+
+#include "../runtime.h"
+
+azt_val * math_abs(azt_val *, azt_args *, eval_state *);
+azt_val * math_acos(azt_val *, azt_args *, eval_state *);
+azt_val * math_asin(azt_val *, azt_args *, eval_state *);
+azt_val * math_atan(azt_val *, azt_args *, eval_state *);
+azt_val * math_atan2(azt_val *, azt_args *, eval_state *);
+azt_val * math_ceil(azt_val *, azt_args *, eval_state *);
+azt_val * math_cos(azt_val *, azt_args *, eval_state *);
+azt_val * math_exp(azt_val *, azt_args *, eval_state *);
+azt_val * math_floor(azt_val *, azt_args *, eval_state *);
+azt_val * math_log(azt_val *, azt_args *, eval_state *);
+azt_val * math_max(azt_val *, azt_args *, eval_state *);
+azt_val * math_min(azt_val *, azt_args *, eval_state *);
+azt_val * math_pow(azt_val *, azt_args *, eval_state *);
+azt_val * math_random(azt_val *, azt_args *, eval_state *);
+azt_val * math_round(azt_val *, azt_args *, eval_state *);
+azt_val * math_sin(azt_val *, azt_args *, eval_state *);
+azt_val * math_sqrt(azt_val *, azt_args *, eval_state *);
+azt_val * math_tan(azt_val *, azt_args *, eval_state *);
+
+azt_val * bootstrap_math(void);
+
+#endif
